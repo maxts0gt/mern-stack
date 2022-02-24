cb8f1bea4fa3d01e43dcfc74198eb9f2f84eed4e
max-tsogt <max@maxtsogt.com>
2022-02-25 00:00:01 +0900

    error handling

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,6 +1,21 @@
+// @desc   Register a new user
+// @route  /api/users
+// @access Public
+
 const registerUser = (req, res) => {
+  const { name, email, password } = req.body
+
+  // Validation
+  if (!name || !email || !password) {
+    return res.status(400).json({ message: 'Please include all fields' })
+  }
+
   res.send('Register Route')
 }
+
+// @desc   Login user
+// @route  /api/users
+// @access Public
 const loginUser = (req, res) => {
   res.send('Login Route')
 }
