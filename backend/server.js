79218bb7431c8ded6bd04b5cef3a23972e369ae9
max-tsogt <max@maxtsogt.com>
2022-02-25 00:27:27 +0900

    connect to database

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,12 @@
 const express = require('express')
+const colors = require('colors')
 const dotenv = require('dotenv').config()
-const PORT = process.env.PORT || 8000
+const { errorHandler } = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
+const PORT = process.env.PORT || 5000
+
+// Connect to database
+connectDB()
 
 const app = express()
 
@@ -8,10 +14,12 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 app.get('/', (req, res) => {
-  res.status(200).json({ message: 'Welcome to the Support DESK API' })
+  res.status(200).json({ message: 'Welcome to the Support Desk API' })
 })
 
 // Routes
 app.use('/api/users', require('./routes/userRoutes'))
 
-app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
+app.use(errorHandler)
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
