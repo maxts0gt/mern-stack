871ff235d69c0d8e437970bb39324ec5d1a3c25a
max-tsogt <max@maxtsogt.com>
2022-02-24 23:31:00 +0900

    setup express js

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const PORT = process.env.PORT || 8000
+
+const app = express()
+
+app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Welcome to the Support DESK API' })
+})
+
+app.listen(PORT, () => console.log(`Server listening on port ${PORT}`))
